5742f265adfc4f297f2d706af233707eb3a6ed70
karenrni <clairriani@gmail.com>
2025-09-29 23:12:27 -0400

    lab 3

--- lab 3/Template/js/main.js
+++ lab 3/Template/js/main.js
@@ -6,9 +6,12 @@ let width = 700,
 // Load CSV file
 d3.csv("data/wealth_health_data.csv", row => {
 
-	// console.log(row)
+	console.log(row)
 	// TODO: convert values where necessary in this callback (d3.csv reads the csv line by line. In the callback,
 	//  you have access to each line (or row) represented as a js object with key value pairs. (i.e. a dictionary).
+	row.Income = +row.Income;
+    row.LifeExpectancy = +row.LifeExpectancy;
+    row.Population = +row.Population;
 	return row;
 }).then( data => {
 	// Analyze the dataset in the web console
@@ -16,15 +19,61 @@ d3.csv("data/wealth_health_data.csv", row => {
 	console.log("Countries: " + data.length)
 
 	// TODO: sort the data
+	data.sort((a, b) => b.Income - a.Income);
 	// TODO: Call your separate drawing function here, i.e. within the .then() method's callback function
-
+	drawChart(data);
 });
 
 // TODO: create a separate function that is in charge of drawing the data, which means it takes the sorted data as an argument
 
-// function drawChart() {
+function drawChart(data) {
 //
-// }
+	let svg = d3.select("#chart-area")
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	let incomeScale = d3.scaleLinear()
+		.domain([d3.min(data, d => d.Income), d3.max(data, d => d.Income)])
+		.range([0, width]);
+
+	let lifeExpectancyScale = d3.scaleLinear()
+		.domain([d3.min(data, d => d.LifeExpectancy), d3.max(data, d => d.LifeExpectancy)])
+		.range([height, 0]);
+
+	
+		svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => incomeScale(d.Income))
+        .attr("cy", d => lifeExpectancyScale(d.LifeExpectancy))
+        .attr("r", 5)
+        .attr("fill", "blue");
+
+    let xAxis = d3.axisBottom(incomeScale);
+    let yAxis = d3.axisLeft(lifeExpectancyScale);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height - 10)
+        .attr("text-anchor", "middle")
+        .text("Income (GDP per Capita)");
+
+    svg.append("text")
+        .attr("x", -height / 2)
+        .attr("y", 20)
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle")
+        .text("Life Expectancy");
+}
 
 
 // let promiseObject = d3.csv("data/wealth_health_data.csv")
